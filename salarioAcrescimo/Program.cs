608115da9a103fad2f413ffc121636297276caf6
masfazan <msfazan@icloud.com>
2024-04-21 16:10:30 -0300

    Add project files.

---
+++ salarioAcrescimo/Program.cs
@@ -0,0 +1,17 @@
+﻿float salAtual = 0, salNovo = 0, acrescimo = 0;
+
+Console.WriteLine("Digite o salário atual: R$ ");
+salAtual = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o salário com acréscimo: R$ ");
+salNovo = float.Parse(Console.ReadLine());
+
+if (salNovo <= salAtual)
+{
+    Console.WriteLine("O valor do salário com acréscimo deve ser maior que o salário atual");
+}
+else
+{
+    acrescimo = ((salNovo - salAtual) / salAtual * 100);
+    Console.WriteLine("O acréscimo salarial foi de: " + acrescimo + "%");
+}
